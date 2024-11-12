4d0f1a533653533ab408e9018c1fd929409d5251
luoyisen <1402175410@qq.com>
2024-11-12 17:16:04 +0800

    fix ui

--- src/App.vue
+++ src/App.vue
@@ -14,6 +14,11 @@ const routeStack = computed(() => {
   return route.path.split("/").filter((_) => !!_);
 });
 
+const vhistory = ref({});
+const updateHistory = () => {
+  vhistory.value = window.history;
+};
+
 const getData = (
   url = `${baseServer}${(endPoint + route.path).replace("//", "/")}`
 ) => {
@@ -22,6 +27,7 @@ const getData = (
     .then((_) => _.json())
     .then((res) => {
       list.value = res;
+      updateHistory();
     });
 };
 
@@ -68,20 +74,26 @@ const changeMode = () => {
 const isImage = (name) => {
   return /\.jpg|\.jpeg|\.png|\.gif/i.test(name);
 };
-
-const tooltip = ref("");
-const generateTooltip = (item) => {
-  tooltip.value = `${item.name}
-  ${item.size}
-  ${item.mtime}`;
-};
 </script>
 
 <template>
   <div class="page">
-    <div class="tooltip">{{ tooltip }}</div>
+    <div class="total">共{{ list.length }}项</div>
 
     <div class="hd">
+      <div class="routeD">
+        <div
+          class="icon back"
+          :class="{ disabled: !vhistory.state?.back }"
+          @click="router.back"
+        ></div>
+        <div
+          class="icon forward"
+          :class="{ disabled: !vhistory.state?.forward }"
+          @click="router.forward"
+        ></div>
+        <div class="icon refresh" @click="router.go(0)"></div>
+      </div>
       <div class="routeStack">
         <span @click="backToDirectory(-1)">Root</span>
         <span
@@ -102,7 +114,6 @@ const generateTooltip = (item) => {
         v-for="(item, idx) in list"
         :key="idx"
         @click="onclickItem(item)"
-        @hover="generateTooltip(item)"
       >
         <img
           :src="`${baseServer}${endPoint}${route.path}/${item.name}`"
@@ -121,22 +132,58 @@ const generateTooltip = (item) => {
   width: 100vw;
   height: 100vh;
   overflow: hidden;
-  padding: 2rem 1rem;
+  padding: 1rem;
   box-sizing: border-box;
   background-color: #333;
   color: #e6e6e6;
 
+  .total {
+    position: fixed;
+    right: 0.1rem;
+    bottom: 0.1rem;
+    font-size: 0.8rem;
+  }
+
   .hd {
     width: 100%;
-    height: 2.5rem;
+    height: 2rem;
+    margin-bottom: 0.5rem;
     display: flex;
     justify-content: space-between;
     align-items: center;
-    .routeStack {
-      height: 100%;
-      font-weight: bold;
+    .routeD {
+      margin-right: 1rem;
       display: flex;
-      align-items: center;
+      .icon {
+        cursor: pointer;
+        width: 1rem;
+        height: 1rem;
+
+        background-color: transparent;
+        background-repeat: no-repeat;
+        background-position: center center;
+        background-size: 100% 100%;
+        &.back {
+          margin-right: 0.5rem;
+          background-image: url("@/assets/imgs/back.png");
+        }
+        &.forward {
+          background-image: url("@/assets/imgs/back.png");
+          margin-right: 0.5rem;
+          transform: rotateZ(180deg);
+        }
+        &.refresh {
+          background-image: url("@/assets/imgs/refresh.png");
+        }
+        &.disabled {
+          opacity: 0.5;
+          cursor: not-allowed;
+        }
+      }
+    }
+    .routeStack {
+      margin-right: auto;
+      font-weight: bold;
       span {
         font-size: 1rem;
         color: rgba(255, 255, 255, 0.8);
@@ -216,10 +263,14 @@ const generateTooltip = (item) => {
   .grid {
     display: flex;
     flex-wrap: wrap;
+    justify-content: flex-start;
+    align-content: flex-start;
     width: 100%;
     height: calc(100% - 2.5rem);
     overflow: auto;
     .item {
+      flex-grow: 0;
+      flex-shrink: 0;
       cursor: pointer;
       width: 7rem;
       height: auto;
